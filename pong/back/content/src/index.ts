7b168a8c4691c015b49a11b9381a9616432a3562
juan-ant <juanpinpon51@gmail.com>
2025-12-16 15:26:11 +0100

    [FEATURE] Added basic lobby functionality

--- pong/back/content/src/index.ts
+++ pong/back/content/src/index.ts
@@ -1,17 +1,20 @@
 import express, { NextFunction, Request, Response } from "express";
 import { lobbyManager } from "./lobbyManager";
 import { gameManager } from "./gameManager";
+var cors = require('cors');
 
 const app = express();
 const port = 8888;
 
 app.use(express.json());
 
+app.use(cors());
+
 /**
  * Health check
  */
 app.get("/", (req: Request, res: Response) => {
-  res.send({ message: "Game Service Running" });
+	res.send({ message: "Game Service Running" });
 });
 
 /**
@@ -19,51 +22,47 @@ app.get("/", (req: Request, res: Response) => {
  */
 
 app.get("/lobbies", (req: Request, res: Response) => {
-  res.send(lobbyManager.getLobbies());
+	res.send(lobbyManager.getlobbies());
 });
 
 app.post("/lobbies/create", (req: Request, res: Response) => {
-  const { hostId } = req.body;
-  const lobby = lobbyManager.createLobby(hostId);
-  res.send(lobby);
+	const { lobbyId, hostId } = req.body;
+	if (!lobbyManager.add(lobbyId, hostId))
+		res.status(500).json({ message: "Couldn't create lobby." });
+	res.send(lobbyManager.get(lobbyId));
 });
 
 app.post("/lobbies/join", (req: Request, res: Response) => {
-  const { lobbyId, playerId } = req.body;
-  const lobby = lobbyManager.joinLobby(lobbyId, playerId);
-  res.send(lobby);
+	const { lobbyId, hostId } = req.body;
+	if (!lobbyManager.addplayer(lobbyId, hostId))
+		res.status(500).json({ message: "Couldn't join lobby." });
+	res.send(lobbyManager.get(lobbyId));
 });
 
 app.post("/lobbies/leave", (req: Request, res: Response) => {
-  const { lobbyId, playerId } = req.body;
-  res.send(lobbyManager.leaveLobby(lobbyId, playerId));
+	const { lobbyId, hostId } = req.body;
+	if (!lobbyManager.leaveplayer(lobbyId, hostId))
+		res.status(500).json({ message: "Couldn't leave lobby." });
+	res.send(lobbyManager.get(lobbyId));
 });
 
-/**
- * ─── GAME SESSION ROUTES ────────────────────────
- */
-
+/*
 app.post("/game/start", (req: Request, res: Response) => {
-  const { lobbyId } = req.body;
-  const session = gameManager.startGame(lobbyId);
-  res.send(session);
+	const { lobbyId } = req.body;
+	const session = gameManager.startGame(lobbyId);
+	res.send(session);
 });
 
 app.get("/game/state/:sessionId", (req: Request, res: Response) => {
-  const sessionId = req.params.sessionId;
-  res.send(gameManager.getState(sessionId));
+	const sessionId = req.params.sessionId;
+	res.send(gameManager.getState(sessionId));
 });
+*/
 
-/**
- * ─── ERROR HANDLING ─────────────────────────────
- */
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send({ error: err.message });
+	res.status(500).send({ error: err.message });
 });
 
-/**
- * ─── START SERVER ───────────────────────────────
- */
 app.listen(port, () => {
-  console.log(`Game service running on port ${port}`);
+	console.log(`Game service running on port ${port}`);
 });
--- pong/back/content/src/lobbyManager.ts
+++ pong/back/content/src/lobbyManager.ts
@@ -1,7 +1,66 @@
 import { Lobby } from "./types";
 
 class LobbyManager {
-    
+    myMap = new Map<string, Lobby>();
+
+    get(id: string) {
+        return this.myMap.get(id);
+    }
+
+    has(id: string) {
+        return this.myMap.has(id);
+    }
+
+    add(id: string, player: string) {
+        if (!this.myMap.has(id))
+        {
+            const newlobby: Lobby = {
+                id,
+                hostId: player,
+                players: [player],
+                status: "waiting",
+            };
+            this.myMap.set(id, newlobby);
+            return (true);
+        }
+        return (false);
+    }
+
+    addplayer(id: string, player: string) {
+        if (this.myMap.has(id))
+        {
+            if (this.get(id)?.players.includes(player))
+                return (false);
+            this.get(id)?.players.push(player);
+            return (true);
+        }
+        return (false);
+    }
+
+    leaveplayer(id: string, player: string) {
+        const lob = this.get(id);
+
+        if (lob == undefined)
+            return (false);
+        if (!lob.players.includes(player))
+            return (false);
+
+        let torem = lob.players.indexOf(player);
+        if (torem != -1)
+            lob.players.splice(torem, 1);
+        if (lob.hostId == player)
+        {
+            if (lob.players.length != 0)
+                lob.hostId = lob.players[0];
+            else
+                this.myMap.delete(id);
+        }
+        return (true);
+    }
+
+    getlobbies() {
+        return (this.myMap);
+    }
 }
 
 export const lobbyManager = new LobbyManager();
