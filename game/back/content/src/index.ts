2f48fc16195c26274b518a47ab856f583b5fa2c6
juan-ant <juanpinpon51@gmail.com>
2026-01-12 21:24:59 +0100

    [FEATURE] Added early front prototype to test websockets nd lobby functionality.

--- game/back/content/src/index.ts
+++ game/back/content/src/index.ts
@@ -18,32 +18,37 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-	res.send({ message: "Game Service Running" });
+	return (res.send({ message: "Game Service Running" }));
 });
 
 app.get("/lobbies", (req: Request, res: Response) => {
-	res.send(lobbyManager.getlobbies());
+	return (res.send(lobbyManager.getlobbies()));
 });
 
 app.post("/lobbies/create", (req: Request, res: Response) => {
 	const { lobbyId, hostId } = req.body;
 	if (!lobbyManager.add(lobbyId, hostId))
-		res.status(500).json({ message: "Couldn't create lobby." });
-	res.send(lobbyManager.get(lobbyId));
+		return (res.status(500).json({ message: "Couldn't create lobby." }));
+	return (res.send(lobbyManager.get(lobbyId)));
+});
+
+app.post("/lobbies/checkout", (req: Request, res: Response) => {
+	const { lobbyId, hostId } = req.body;
+	return (res.send(lobbyManager.get(lobbyId)));
 });
 
 app.post("/lobbies/join", (req: Request, res: Response) => {
 	const { lobbyId, hostId } = req.body;
 	if (!lobbyManager.addplayer(lobbyId, hostId))
-		res.status(500).json({ message: "Couldn't join lobby." });
-	res.send(lobbyManager.get(lobbyId));
+		return (res.status(500).json({ message: "Couldn't join lobby." }));
+	return (res.send(lobbyManager.get(lobbyId)));
 });
 
 app.post("/lobbies/leave", (req: Request, res: Response) => {
 	const { lobbyId, hostId } = req.body;
 	if (!lobbyManager.leaveplayer(lobbyId, hostId))
-		res.status(500).json({ message: "Couldn't leave lobby." });
-	res.send(lobbyManager.get(lobbyId));
+		return (res.status(500).json({ message: "Couldn't leave lobby." }));
+	return (res.send(lobbyManager.get(lobbyId)));
 });
 
 /*
@@ -61,10 +66,6 @@ app.get("/game/state/:sessionId", (req: Request, res: Response) => {
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	res.status(500).send({ error: err.message });
-});
-
-app.listen(port, () => {
-	console.log(`Game service running on port ${port}`);
 });
 
 wss.on("connection", (ws: WebSocket, req) => {
@@ -88,4 +89,8 @@ wss.on("connection", (ws: WebSocket, req) => {
     lobbyManager.deletews(userId, ws);
     console.log(`User ${userId} disconnected`);
   });
+});
+
+server.listen(port, () => {
+	console.log(`Game service running on port ${port}`);
 });
---
+++ game/front/my-app/src/App.js
@@ -0,0 +1,279 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useRef, useState } from "react";
+
+let address = window.location.host;
+let noport = "";
+
+if (address.includes(":"))
+	noport = address.split(":")[0];
+
+const wsbase = `ws://${noport}:8888`;
+const apiBase = `http://${noport}:8888`;
+
+function Log({ user, setUser }) {
+	let content;
+
+	if (!user) {
+		content = (
+  			<form
+    			onSubmit={(e) => {
+      			e.preventDefault();
+      			setUser(e.target.query.value);
+    		}}>
+    			<input name="query" placeholder="Enter user" />
+    			<button type="submit">Set</button>
+  			</form>
+	);
+	} else {
+		content = <p>Current user: <strong>{user}</strong></p>;
+	}
+
+	return content;
+}
+
+function Lob({ lobby, setLobby }) {
+	let content;
+
+	if (!lobby) {
+		content = (
+  			<form
+    			onSubmit={(e) => {
+      			e.preventDefault();
+      			setLobby(e.target.query.value);
+    		}}>
+    			<input name="query" placeholder="Enter lobby" />
+    			<button type="submit">Set</button>
+  			</form>
+		);
+	} else {
+		content = (
+			<div>
+  				<form
+    				onSubmit={(e) => {
+      				e.preventDefault();
+      				setLobby(e.target.query.value);
+    			}}>
+    				<input name="query" placeholder="Enter lobby" />
+    				<button type="submit">Set</button>
+  				</form>
+				<div>Current lobby: {lobby}</div>
+			</div>
+		);
+	}
+
+	return content;
+}
+
+function Lobbyes({ response, setResponse }) {
+	if (!response?.all)
+		return (<p>No lobbies available</p>);
+
+	return (
+		<div id="lobbyes-box">
+		{response.all.map((lobbyItem) => (
+        	<div class="lobby" key={lobbyItem.id}>
+        		<div><strong>Name:</strong> {lobbyItem.id}</div>
+        		<div><strong>Host:</strong> {lobbyItem.hostId}</div>
+        		<div><strong>Players:</strong> {lobbyItem.players.length}</div>
+        		<div><strong>Spectators:</strong> {lobbyItem.spectators.length}</div>
+        		<div><strong>Status:</strong> {lobbyItem.status}</div>
+			</div>
+		))}
+    	</div>
+	);
+}
+
+function Lobby({ response }) {
+	return (
+		<div id="players-box">
+        	<div><strong>Name:</strong> {response.id}</div>
+        	<div><strong>Host:</strong> {response.hostId}</div>
+   			<div>
+        	<strong>Players:</strong>
+			<div id="players">
+        	{response.players.map((p) => <div key={p} class="player">{p}</div>)}
+      		</div>
+			</div>
+   			<div>
+        	<strong>Spectators:</strong>
+			<div class="spectators">
+        	<p>{response.spectators.map((p) => `${p}     `)}</p>
+			</div>
+      		</div>
+        	<div><strong>Status:</strong> {response.status}</div>
+		</div>
+	);
+}
+
+function App() {
+	const [lobby, setLobby] = useState("");
+	const [clobby, setClobby] = useState("");
+	const [user, setUser] = useState("");
+	const [ws, setWs] = useState(null);
+	const [response, setResponse] = useState("");
+	const clobbyRef = useRef(clobby);
+
+	useEffect(() => {
+		clobbyRef.current = clobby;
+	}, [clobby]);
+
+	async function createLobby() {
+		try {
+				const res = await fetch(`${apiBase}/lobbies/create`, {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ lobbyId: lobby, hostId: user })
+				});
+				if 	(!res.ok) {
+					const text = await res.text();
+    				throw new Error(`HTTP ${res.status}: ${text}`);
+    			}
+				const data = await res.json();
+				setClobby(data.id);
+				setResponse(data);
+			}
+		catch (error)
+		{
+			console.log(error);
+		}
+	}
+
+	async function stateLobby() {
+		try {
+			const res = await fetch(`${apiBase}/lobbies/checkout`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ lobbyId: clobbyRef.current, hostId: user })
+			});
+			if (!res.ok) {
+				const text = await res.text();
+    			throw new Error(`HTTP ${res.status}: ${text}`);
+    		}
+			const data = await res.json();
+			setResponse(data);
+		}
+		catch (error)
+		{
+			console.log(error);
+		}
+	}
+
+	async function joinLobby() {
+		try {
+			const res = await fetch(`${apiBase}/lobbies/join`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ lobbyId: lobby, hostId: user  })
+			});
+			if (!res.ok) {
+    			const text = await res.text();
+    			throw new Error(`HTTP ${res.status}: ${text}`);
+    		}
+			const data = await res.json();
+			setClobby(data.id);
+			setResponse(data);
+		}
+		catch (error)
+		{
+			console.log(error);
+		}
+	}
+
+	async function listLobbies() {
+		const res = await fetch(`${apiBase}/lobbies`);
+		const data = await res.json();
+		setResponse(data);
+	}
+
+	async function leaveLobby() {
+		try {
+			const res = await fetch(`${apiBase}/lobbies/leave`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ lobbyId: clobby, hostId: user })
+			});
+			const data = await res.json();
+			setClobby("");
+			listLobbies();
+		}
+		catch (error)
+		{
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+    	if (!user) return;
+	
+    	const socket = new WebSocket(`${wsbase}?userId=${user}`);
+
+    	socket.onopen = () => {
+      		console.log("WebSocket connected as", user);
+    	};
+
+		socket.onmessage = (event) => {
+			const msg = JSON.parse(event.data);
+
+			if (msg.type == "LOBBYUPDATE")
+			{
+				console.log(`${msg.lobby} ${msg.user} ${msg.action}`)
+				stateLobby();
+			}	
+		};
+
+		socket.onclose = () => console.log("WebSocket disconnected");
+
+		setWs(socket);
+
+		return () => {
+			socket.close();
+		};
+	}, [user]);
+
+
+	useEffect(() => {
+		listLobbies();
+	}, [])
+
+	return (
+	<div>
+		<h1>Lobby Test</h1>
+
+		<Log user={user} setUser={setUser}/>
+		<Lob lobby={lobby} setLobby={setLobby}/>
+		{user && lobby && !clobby &&
+		
+			<div>
+				<h2>Create Lobby</h2>
+				<button onClick={createLobby}>Create Lobby</button>
+				<h2>Join Lobby</h2>
+				<button onClick={joinLobby}>Join Lobby</button>
+			</div>
+		}
+		{!clobby && (!user || !lobby) && <div>Set user and lobby to access all lobby functionality.</div> }
+		{clobby ?
+			<div>
+				<h2>Leave Lobby</h2>
+				<button onClick={leaveLobby}>Leave Lobby</button>
+
+				<h2>Response</h2>
+				<Lobby response={response} setResponse={setResponse}/>
+			</div>
+		:
+			<div>
+				<h2>List Lobbies</h2>
+				<button onClick={listLobbies}>Refresh List</button>
+				{response &&
+					<div>
+						<h2>Response</h2>
+						<Lobbyes response={response} setResponse={setResponse}/>
+					</div>
+				}
+			</div>
+		}
+	</div>
+	);
+}
+
+export default App;
