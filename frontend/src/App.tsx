6594503cd7ce40299b2ebf3d9f19de2afdc0b4c4
root <root@LAPTOP-DRE6KOQ0.localdomain>
2026-01-20 20:29:25 +0100

    [FRONT][CHORE] initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold text-white">Transcendence</h1>
+    </div>
+  )
+}
+
+export default App
