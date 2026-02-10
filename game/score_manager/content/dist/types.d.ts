ebe4d1e69d18edef5bc2abcdac50d053d45e43f8
juan-ant <juanpinpon51@gmail.com>
2026-02-10 20:29:52 +0100

    Remove dist folders from repo

--- game/score_manager/content/dist/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface GameResults {
-    first: string;
-    second: string;
-    third: string;
-    fourth: string;
-}
-export declare enum Errors {
-}
-//# sourceMappingURL=types.d.ts.map
